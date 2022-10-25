c442c9b2c85e3ec7e86340fbd64153e40d481fe7
Kader Bouyakoub <bkader@mail.com>
2022-10-25 14:26:43 +0100

    moved cc spells to Tables.lua and & metatables
    + added a global "inCombat" flag to prevent certain modules from perfoming scan at the start of the combat, to prevent any peformance hit.
    + cleaning

--- Skada/Modules/Absorbs.lua
+++ Skada/Modules/Absorbs.lua
@@ -11,7 +11,7 @@ local hits_perc = "%s (\124cffffffff%s\124r)"
 -- Absorbs module --
 -- ============== --
 
-Skada:RegisterModule("Absorbs", function(L, P)
+Skada:RegisterModule("Absorbs", function(L, P, G)
 	local mod = Skada:NewModule("Absorbs")
 	local spellmod = mod:NewModule("Absorb spell list")
 	local targetmod = mod:NewModule("Absorbed target list")
@@ -88,10 +88,10 @@ Skada:RegisterModule("Absorbs", function(L, P)
 		[39228] = 0x02, -- Argussian Compass
 		[40322] = 0x10, -- Teron's Vengeful Spirit Ghost - Spirit Shield
 		[42137] = 0x01, -- Greater Rune of Warding
-		[47509] = 0x01, -- Divine Aegis (rank 1)
-		[47511] = 0x01, -- Divine Aegis (rank 2)
-		[47515] = 0x01, -- Divine Aegis (rank 3)
-		[47753] = 0x01, -- Divine Aegis (rank 1)
+		[47509] = 0x02, -- Divine Aegis (rank 1)
+		[47511] = 0x02, -- Divine Aegis (rank 2)
+		[47515] = 0x02, -- Divine Aegis (rank 3)
+		[47753] = 0x02, -- Divine Aegis (rank 1)
 		[47788] = 0x02, -- Guardian Spirit
 		[48707] = 0x20, -- Anti-Magic Shell
 		[51052] = 0x20, -- Anti-Magic Zone
@@ -100,7 +100,7 @@ Skada:RegisterModule("Absorbs", function(L, P)
 		[53913] = 0x10, -- Mighty Frost Protection Potion
 		[53914] = 0x08, -- Mighty Nature Protection Potion
 		[53915] = 0x20, -- Mighty Shadow Protection Potion
-		[54704] = 0x01, -- Divine Aegis (rank 1)
+		[54704] = 0x02, -- Divine Aegis (rank 1)
 		[54808] = 0x01, -- Noise Machine (onic Shield)
 		[55019] = 0x01, -- Sonic Shield
 		[57350] = 0x01, -- Darkmoon Card: Illusion
@@ -158,7 +158,7 @@ Skada:RegisterModule("Absorbs", function(L, P)
 		local actor = Skada:GetPlayer(set, absorb.actorid, absorb.actorname)
 		if not actor then
 			return
-		elseif actor.role ~= "DAMAGER" and not passive_spells[absorb.spellid] and not nocount then
+		elseif actor.role ~= "DAMAGER" and not passive_spells[absorb.spell] and not nocount then
 			Skada:AddActiveTime(set, actor, absorb.dstName)
 		end
 
@@ -208,13 +208,8 @@ Skada:RegisterModule("Absorbs", function(L, P)
 
 		-- record the target
 		if not absorb.dstName then return end
-		local target = spell.targets and spell.targets[absorb.dstName]
-		if not target then
-			spell.targets = spell.targets or {}
-			spell.targets[absorb.dstName] = 0
-			target = spell.targets[absorb.dstName]
-		end
-		target = target + absorb.amount
+		spell.targets = spell.targets or {}
+		spell.targets[absorb.dstName] = (spell.targets[absorb.dstName] or 0) + absorb.amount
 	end
 
 	-- https://github.com/TrinityCore/TrinityCore/blob/5d82995951c2be99b99b7b78fa12505952e86af7/src/server/game/Spells/Auras/SpellAuraEffects.h#L316
@@ -445,6 +440,7 @@ Skada:RegisterModule("Absorbs", function(L, P)
 			absorb.dstName = dstName
 			absorb.dstFlags = dstFlags
 
+			absorb.spell = shield.spellid
 			absorb.spellid = shield.string
 			absorb.string = shield.string
 			absorb.amount = absorbed
@@ -522,8 +518,8 @@ Skada:RegisterModule("Absorbs", function(L, P)
 		win.title = L["actor absorb spells"](win.actorname or L["Unknown"], win.targetname or L["Unknown"])
 		if not set or not win.targetname then return end
 
-		local actor, enemy = set:GetActor(win.actorname, win.actorid)
-		if not actor or enemy then return end -- unavailable for enemies yet
+		local actor = set:GetActor(win.actorname, win.actorid)
+		if not actor or actor.enemy then return end -- unavailable for enemies yet
 
 		local total = actor.absorb
 		local spells = (total and total > 0) and actor.absorbspells
@@ -557,8 +553,8 @@ Skada:RegisterModule("Absorbs", function(L, P)
 		win.title = L["actor absorb spells"](win.actorname or L["Unknown"])
 		if not set or not win.actorname then return end
 
-		local actor, enemy = set:GetActor(win.actorname, win.actorid)
-		if not actor or enemy then return end -- unavailable for enemies yet
+		local actor = set:GetActor(win.actorname, win.actorid)
+		if not actor or actor.enemy then return end -- unavailable for enemies yet
 
 		local total = actor.absorb
 		local spells = (total and total > 0) and actor.absorbspells
@@ -589,8 +585,8 @@ Skada:RegisterModule("Absorbs", function(L, P)
 		win.title = uformat(L["%s's absorbed targets"], win.actorname)
 		if not set or not win.actorname then return end
 
-		local actor, enemy = set:GetActor(win.actorname, win.actorid)
-		if not actor or enemy then return end -- unavailable for enemies yet
+		local actor = set:GetActor(win.actorname, win.actorid)
+		if not actor or actor.enemy then return end -- unavailable for enemies yet
 
 		local total = actor and actor.absorb or 0
 		local targets = (total > 0) and actor:GetAbsorbTargets(set)
@@ -680,7 +676,7 @@ Skada:RegisterModule("Absorbs", function(L, P)
 		end
 
 		function mod:CombatEnter(_, set, args)
-			if set and not set.stopped and not self.checked then
+			if not G.inCombat and set and not set.stopped and not self.checked then
 				GroupIterator(check_unit_shields, args.timestamp, set.last_time or GetTime())
 				self.checked = true
 			end
@@ -887,7 +883,7 @@ Skada:RegisterModule("Absorbs and Healing", function(L, P)
 		win.title = L["actor absorb and heal spells"](win.actorname or L["Unknown"], win.targetname or L["Unknown"])
 		if not set or not win.targetname then return end
 
-		local actor, enemy = set:GetActor(win.actorname, win.actorid)
+		local actor = set:GetActor(win.actorname, win.actorid)
 		local total = actor and actor:GetAbsorbHealOnTarget(win.targetname)
 
 		if not total or total == 0 or not (actor.healspells or actor.absorbspells) then
@@ -903,7 +899,7 @@ Skada:RegisterModule("Absorbs and Healing", function(L, P)
 		if spells then
 			for spellid, spell in pairs(spells) do
 				local amount = spell.targets and spell.targets[win.targetname]
-				amount = amount and (enemy and amount or amount.amount)
+				amount = amount and (actor.enemy and amount or amount.amount)
 				if amount then
 					nr = nr + 1
 
