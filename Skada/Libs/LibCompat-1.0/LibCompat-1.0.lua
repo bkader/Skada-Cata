496e1c9400ef5be8cceb07c98ae17b6b6f67676b
Kader Bouyakoub <bkader@mail.com>
2023-07-27 23:36:34 +0100

    Update LibCompat-1.0.lua

--- Skada/Libs/LibCompat-1.0/LibCompat-1.0.lua
+++ Skada/Libs/LibCompat-1.0/LibCompat-1.0.lua
@@ -405,69 +405,77 @@ do
 	end
 
 	-- cached specs
-	local cachedSpecs = setmetatable({}, {__index = function(self, guid)
-		local unit = guid and GetUnitIdFromGUID(guid, true)
-		if not unit then return end
+	local GetUnitSpec = setmetatable({}, {
+		__index = function(self, guid)
+			local unit = guid and GetUnitIdFromGUID(guid, true)
+			if not unit then return end
 
-		local _, class = UnitClass(unit)
-		if not class or not specsTable[class] then return end
+			local _, class = UnitClass(unit)
+			if not class or not specsTable[class] then return end
 
-		local talentGroup = LGT:GetActiveTalentGroup(unit)
-		local maxPoints, index = 0, 0
+			local talentGroup = LGT:GetActiveTalentGroup(unit)
+			local maxPoints, index = 0, 0
 
-		for i = 1, MAX_TALENT_TABS do
-			local _, _, _, _, pointsSpent = LGT:GetTalentTabInfo(unit, i, talentGroup)
-			if pointsSpent ~= nil then
-				if maxPoints < pointsSpent then
-					maxPoints = pointsSpent
-					if class == "DRUID" and i >= 2 then
-						if i == 3 then
-							index = 4
-						elseif i == 2 then
-							index = GetFeralSubSpec(unit, talentGroup)
+			for i = 1, MAX_TALENT_TABS do
+				local _, _, _, _, pointsSpent = LGT:GetTalentTabInfo(unit, i, talentGroup)
+				if pointsSpent ~= nil then
+					if maxPoints < pointsSpent then
+						maxPoints = pointsSpent
+						if class == "DRUID" and i >= 2 then
+							if i == 3 then
+								index = 4
+							elseif i == 2 then
+								index = GetFeralSubSpec(unit, talentGroup)
+							end
+						else
+							index = i
 						end
-					else
-						index = i
 					end
 				end
 			end
-		end
 
-		local spec = specsTable[class][index]
-		rawset(self, guid, spec)
-		return spec
-	end})
+			local spec = specsTable[class][index]
+			rawset(self, guid, spec)
+			return spec
+		end,
+		__newindex = function(self, guid, spec)
+			rawset(self, guid, spec)
+		end,
+		__call = function(self, guid)
+			return self[guid]
+		end
+	})
 
 	-- cached roles
-	local cachedRoles = setmetatable({}, {__index = function(self, guid)
-		local unit = guid and GetUnitIdFromGUID(guid, true)
-		if not unit then return end
+	local GetUnitRole = setmetatable({}, {
+		__index = function(self, guid)
+			local unit = guid and GetUnitIdFromGUID(guid, true)
+			if not unit then return end
 
-		local role = UnitGroupRolesAssigned(unit)
-		if role and role ~= "NONE" then
+			local role = UnitGroupRolesAssigned(unit)
+			if role and role ~= "NONE" then
+				rawset(self, guid, role)
+				return role
+			end
+
+			local _, class = UnitClass(unit)
+			-- speedup things using classes.
+			if class == "HUNTER" or class == "MAGE" or class == "ROGUE" or class == "WARLOCK" then
+				role = "DAMAGER"
+			else
+				role = LGTRoleTable[LGT:GetUnitRole(unit)] or "NONE"
+			end
+
 			rawset(self, guid, role)
 			return role
+		end,
+		__newindex = function(self, guid, role)
+			rawset(self, guid, role)
+		end,
+		__call = function(self, guid)
+			return self[guid]
 		end
-
-		local _, class = UnitClass(unit)
-		-- speedup things using classes.
-		if class == "HUNTER" or class == "MAGE" or class == "ROGUE" or class == "WARLOCK" then
-			role = "DAMAGER"
-		else
-			role = LGTRoleTable[LGT:GetUnitRole(unit)] or "NONE"
-		end
-
-		rawset(self, guid, role)
-		return role
-	end})
-
-	local function GetUnitSpec(guid)
-		return cachedSpecs[guid]
-	end
-
-	local function GetUnitRole(guid)
-		return cachedRoles[guid]
-	end
+	})
 
 	LGT:RegisterCallback("LibGroupTalents_Update", function(_, guid, unit, _, n1, n2, n3)
 		if not guid or not unit then return end
@@ -484,13 +492,13 @@ do
 				index = (points and points > 0) and 3 or 2
 			end
 
-			cachedSpecs[guid] = specsTable[class][index]
+			GetUnitSpec[guid] = specsTable[class][index]
 		end
 	end)
 
 	LGT:RegisterCallback("LibGroupTalents_RoleChange", function(_, guid, _, role, oldrole)
 		if not guid or role == oldrole then return end
-		cachedRoles[guid] = LGTRoleTable[role] or role
+		GetUnitRole[guid] = LGTRoleTable[role] or role
 	end)
 
 	lib.GetUnitSpec = GetUnitSpec
