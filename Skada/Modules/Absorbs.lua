99c173945f5928919ba1ddcaac159cbb7a39deaa
Kader Bouyakoub <bkader@mail.com>
2023-08-06 10:39:20 +0100

    small "format" changes
    - Use of string.format.
    - "Interface\Icons" to "Interface\ICONS"

--- Skada/Modules/Absorbs.lua
+++ Skada/Modules/Absorbs.lua
@@ -639,7 +639,7 @@ Skada:RegisterModule("Absorbs", function(L, P, G)
 			click1 = mode_spell,
 			click2 = mode_target,
 			columns = {Absorbs = true, APS = true, Percent = true, sAPS = false, sPercent = true},
-			icon = [[Interface\Icons\spell_holy_devineaegis]]
+			icon = [[Interface\ICONS\spell_holy_devineaegis]]
 		}
 
 		mode_cols = self.metadata.columns
@@ -1013,7 +1013,7 @@ Skada:RegisterModule("Absorbs and Healing", function(L, P)
 			click2 = mode_target,
 			post_tooltip = hps_tooltip,
 			columns = {Healing = true, HPS = true, Percent = true, sHPS = false, sPercent = true},
-			icon = [[Interface\Icons\spell_holy_healingfocus]]
+			icon = [[Interface\ICONS\spell_holy_healingfocus]]
 		}
 	end
 end, "Absorbs", "Healing")
@@ -1096,7 +1096,7 @@ Skada:RegisterModule("HPS", function(L, P)
 			filterclass = true,
 			tooltip = hps_tooltip,
 			columns = {HPS = true, Percent = true},
-			icon = [[Interface\Icons\spell_nature_rejuvenation]]
+			icon = [[Interface\ICONS\spell_nature_rejuvenation]]
 		}
 
 		mode_cols = self.metadata.columns
@@ -1250,7 +1250,7 @@ Skada:RegisterModule("Healing Done By Spell", function(L, _, _, C)
 		self.metadata = {
 			click1 = mode_source,
 			columns = {Healing = true, HPS = false, Percent = true, sHPS = false, sPercent = true},
-			icon = [[Interface\Icons\spell_nature_healingwavelesser]]
+			icon = [[Interface\ICONS\spell_nature_healingwavelesser]]
 		}
 		mode_cols = self.metadata.columns
 		Skada:AddMode(self, "Absorbs and Healing")
