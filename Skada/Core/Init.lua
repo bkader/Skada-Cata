e0e87d1ff5a1ae5027225e0d2329b656a6331ecb
Kader Bouyakoub <bkader@mail.com>
2023-11-16 16:06:05 +0100

    remove realm name by default
    Changes should also fix cross-realm stuff (hopefully)

--- Skada/Core/Init.lua
+++ Skada/Core/Init.lua
@@ -1481,6 +1481,11 @@ do
 	local BITMASK_NPC = Private.BITMASK_NPC
 	local BITMASK_PLAYER = Private.BITMASK_PLAYER
 
+	ns.userGUID = UnitGUID("player")
+	_, ns.userClass = UnitClass("player")
+	ns.userName = UnitName("player")
+	ns.userRealm = gsub(GetRealmName(), "%s", "")
+
 	-- checks if the given guid/flags are those of a creature.
 	function Private.IsCreature(guid, flags)
 		if tonumber(guid) then
@@ -1622,10 +1627,6 @@ do
 		guidToClass[guid] = class
 		guidToName[guid] = UnitFullName(unit)
 	end
-
-	ns.userGUID = UnitGUID('player')
-	_, ns.userClass = UnitClass('player')
-	ns.userName = UnitFullName("player")
 end
 
 -------------------------------------------------------------------------------
